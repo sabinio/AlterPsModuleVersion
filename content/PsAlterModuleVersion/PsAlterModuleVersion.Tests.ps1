76cd18c4b10c8b5472450f8ea00563dda99a8c69
Unknown <richie@bzzzt.io>
2017-11-22 23:45:33 +0000

    add checks; add tests for checks
    add tests and tests for checks to run on build prior to creating package

---
+++ content/PsAlterModuleVersion/PsAlterModuleVersion.Tests.ps1
@@ -0,0 +1,53 @@
+# test one - as expected 
+$OldErrorActionPref = $ErrorActionPreference
+$qwerty = ".\PsAlterModuleVersion.ps1"
+$testBuildNumber = "1.5.4567"
+$file = Get-ChildItem ".\TestPsd1file.psd1"
+$fileFullName = $file.Fullname
+$e = . $qwerty -buildNumber $testbuildNumber -File $fileFullName
+Write-Host $e
+if ($e -ne "ModuleVersion = '1.5.4567'") {
+    Throw "oh dear"
+}
+# test two - not a psd1 file
+$ErrorActionPreference = 'SilentlyContinue'
+$testBuildNumber = "1.5.4567"
+$file = Get-ChildItem ".\icon.png"
+$fileFullName = $file.Fullname
+try {
+    . $qwerty -buildNumber $testbuildNumber -File $fileFullName
+}
+catch {
+    $ErrorActionPreference = $OldErrorActionPref
+    if ($_.Exception.Message -eq "notapsd1") {
+        Write-Host "Asserted Exception Correctly - $($_.Exception.Message)"
+    }
+}
+# test 3 - psd1 file does not exist
+$ErrorActionPreference = 'SilentlyContinue'
+$testBuildNumber = "1.5.4567"
+$file = ".\noFile.psd1"
+try {
+    . $qwerty -buildNumber $testbuildNumber -File $file
+}
+catch {
+    $ErrorActionPreference = $OldErrorActionPref
+    $_.Exception.Message
+    if ($_.Exception.Message -eq "psd1miss") {
+        Write-Host "Asserted Exception Correctly - $($_.Exception.Message)"
+    }
+}
+# test four - psd1 file does not contain ModuleVersionNumber
+$ErrorActionPreference = 'SilentlyContinue'
+$testBuildNumber = "1.5.4567"
+$file = Get-ChildItem ".\MissingModuleVersionNumber.psd1"
+$fileFullName = $file.Fullname
+try {
+    . $qwerty -buildNumber $testbuildNumber -File $fileFullName
+}
+catch {
+    $ErrorActionPreference = $OldErrorActionPref
+    if ($_.Exception.Message -eq "NoModuleVersionNumber") {
+        Write-Host "Asserted Exception Correctly - $($_.Exception.Message)"
+    }
+}
--- content/PsAlterModuleVersion/PsAlterModuleVersion.ps1
+++ content/PsAlterModuleVersion/PsAlterModuleVersion.ps1
@@ -1,26 +1,49 @@
-param(
+
+param(    
     $buildNumber,
     $file
 )
-$FileExists = Test-Path $file
-If ($FileExists -eq $false) {
-    Write-Error "$File does not exist!"
-    Throw
+
+Function Edit-ModuleVersionNumber {
+    [cmdletbinding()]
+    param(
+        [ValidateNotNullOrEmpty()]
+        $ModuleVersionNumber,
+        [ValidateNotNullOrEmpty()]
+        $psd1File
+    )
+    if ( $(Try { Test-Path $psd1File.trim() } Catch { $false }) ) {
+        write-host "Path $psd1File OK"
+    }
+    Else {
+        Write-Error "$psd1File does not exist!"
+        Throw "psd1miss"
+    }
+    $extn = [IO.Path]::GetExtension($psd1File)
+    if ($extn -ne ".psd1" ) {
+        Write-Error "$psd1File is not a psd1 psd1File!"
+        Throw "notapsd1"
+    }
+    $psd1FileName = Split-Path -Path $psd1File -Leaf
+    Write-Host "$psd1FileName location is $psd1File"
+    $regex = 'ModuleVersion(.*)'
+    $ReturnValue = (@( Get-Content $psd1File | Where-Object { $_.Contains("ModuleVersion") } ).Count)
+    if ($ReturnValue -eq 0) {
+        Write-Error "ModuleVersionNumber element not found in $psd1FileName!"
+        Throw "NoModuleVersionNumber"
+    }
+    try {
+        Write-Host "ModuleVersionNumber in $psd1FileName will be altered to $ModuleVersionNumber."
+        (Get-Content $psd1File) -replace $regex, "ModuleVersion = '$ModuleVersionNumber'" | Set-Content $psd1File
+        [string]$updatedModuleVersion = Get-Content $psd1File | Where-Object { $_ -match "ModuleVersion" }
+        $updatedModuleVersion = $updatedModuleVersion.Trim()
+        Write-Host "Updated to $updatedModuleVersion"
+        Return $updatedModuleVersion
+    }
+    catch {
+        Write-Error "Something went wrong in trying to update ModuleNumber."
+        Throw 0
+    }
 }
-$extn = [IO.Path]::GetExtension($file)
-if ($extn -ne ".psd1" )
-{
-    Write-Error "$file is not a psd1 file!"
-    Throw
-}
-Write-Host "psd1 location is $file"
-Write-Host "ModuleVersionNumber will be altered to $buildNumber"
-$regex = 'ModuleVersion(.*)'
-try {
-    (Get-Content $file) -replace $regex, "ModuleVersion = '$buildNumber'" | Set-Content $file
-    Write-Host "Updated."
-}
-catch {
-    Write-Error "Something went wrong in trying to update ModuleNumber."
-    Throw
-}
+Write-Host "here "$file
+Edit-ModuleVersionNumber -ModuleVersionNumber $buildNumber -psd1File $file
